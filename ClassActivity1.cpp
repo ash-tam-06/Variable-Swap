64101480692a7ac01e323ef176b30ba0a6c8bb47
Asmita Tamang <asmitatam2006@gmail.com>
2025-01-26 21:02:53 -0800

    testing commit

---
+++ ClassActivity1.cpp
@@ -0,0 +1,11 @@
+//Swap values of variables
+
+#include "ClassActivity1.h"
+#inclue <iostream>
+using namespace std;
+
+int main(){
+  cout<<"Hello World!"<<endl;
+  return 0;
+
+}
---
+++ ClassActivity1.h
@@ -0,0 +1,16 @@
+//
+// Created by Asmita Tamang on 1/26/25.
+//
+
+#ifndef CLASSACTIVITY1_H
+#define CLASSACTIVITY1_H
+
+
+
+class ClassActivity1 {
+
+};
+
+
+
+#endif //CLASSACTIVITY1_H
